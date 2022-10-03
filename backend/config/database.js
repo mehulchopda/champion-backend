3c71194ee315f3139ea4221bcff3ad21008f063f
chopada <mehul.chopada@webfleet.com>
2022-10-03 19:50:42 +0200

    Initial commit

---
+++ backend/config/database.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      ssl: env('DATABASE_SSL', false)
+    },
+    debug: false,
+  },
+});
